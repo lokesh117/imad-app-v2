2553b861348cec1f0c245b5a069a15b74db34789
lokesh117 <lokesh.narsepalli@gmail.com>
2017-02-11 18:04:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'artcle-one.html'));
+app.get('/article-2',function(req,res){
+     res.sendFile(path.join(__dirname, 'ui', 'artcle-2.html'));
     
 });
 
